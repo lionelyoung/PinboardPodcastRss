53b23370614748ecdc840322761e8c8ef34949ff
Lionel <lionelyoung@gmail.com>
2015-01-01 19:00:54 -0800

    Add code and README

---
+++ README.markdown
@@ -0,0 +1,21 @@
+# Config
+
+The config.py contains your Pinbaord API token, so it can grab the recent
+posts.  Use `limit_tags` to filter certain tags
+
+1. Visit https://pinboard.in/settings/password to get your API token
+2. Create file `config.py` (see `config.py.example`) with `api_token` and `limit_tags`
+	api_token = "jsmith:9ABC8DEF7GHI6JKL5MNO"
+	limit_tags = ["mp3"]
+	output = "pinduff.rss"
+
+# Usage
+
+Dependencies are feedgen and pinboard.  See pip-requirements.txt
+
+1. To install dependencies: pip install -r pip-requirements.txt
+2. Run: python pbpodcast.py
+
+Now upload your rss file (default is `pinduff.rss`) where you want it.  For example:
+
+	rsync -avzue ssh pinduff.rss myserver:/srv/www/mysite/
---
+++ pbpodcast.py
@@ -0,0 +1,36 @@
+from feedgen.feed import FeedGenerator
+from pinboard import Pinboard
+from config import api_token, limit_tags, output
+
+if __name__ == "__main__":
+    # create the feed
+    fg = FeedGenerator()
+    fg.id('pbpodcast123')
+    fg.title('Pinboard RSS')
+    fg.author({'name': 'John Smith', 'email': 'john@example.com'})
+    fg.link(href='http://example.com', rel='alternate')
+    fg.logo('http://example.com/logo.jpg')
+    fg.subtitle('This is a cool feed!')
+    fg.link(href='http://example.com/pinduff.rss', rel='self')
+    fg.language('en')
+
+    # podcast
+    fg.load_extension('podcast')
+    fg.podcast.itunes_category('Technology', 'Podcasting')
+
+    # get recent posts from pinboard
+    pb = Pinboard(api_token)
+    bookmarks = pb.posts.recent(tag=limit_tags)
+
+    # add feed entries only if they are mp3s
+    for bm in bookmarks['posts']:
+        if 'mp3' in bm.url:
+            fe = fg.add_entry()
+            fe.id(bm.url)
+            fe.title(bm.description)
+            fe.description(bm.extended)
+            fe.enclosure(bm.url, 0, "audio/mpeg")
+
+    # generate feed
+    fg.rss_str(pretty=True)
+    fg.rss_file(output)
